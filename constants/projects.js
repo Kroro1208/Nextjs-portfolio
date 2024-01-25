e49cf9f1bcad5dcc59cc9b1e26d1183866a1d759
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-01-25 20:47:20 +0900

    cardに表示させるdataを作成(constants/projects.js)

---
+++ constants/projects.js
@@ -0,0 +1,76 @@
+export const projects = [
+    {
+        name: "フリーランスホームページ",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/Designer.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "働き方メディア",
+        github: "",
+        image: "/public/asetts/projects/Marketing-bro.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "マーケティング戦略",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/Marketing.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "学生向けメディア",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/Students-cuate.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "ビジネスブランディングページ",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/rebranding-bro.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "ポートフォリオ",
+        github: "https://github.com/Kroro1208",
+        image: "",
+        category: ["react", "aws"],
+        projectUrl: "/public/asetts/projects/Portfolio.png",
+        deployed: true
+    },
+    {
+        name: "化粧品メディア",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/Waterproof.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "ブログメディア",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/Post-cuate.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "商品紹介",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/Product.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "企業ホームページ",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/Law.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "ネット配信ページ",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/Online.png",
+        category: ["react", "aws"]
+    },
+    {
+        name: "文化祭ページ",
+        github: "https://github.com/Kroro1208",
+        image: "/public/asetts/projects/Celebration-cuate.png",
+        category: ["react", "aws"]
+    },
+];
