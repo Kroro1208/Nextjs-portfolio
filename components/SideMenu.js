4afe6fb01205480bae00dffad0548bcd505f9593
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-01-28 15:00:26 +0900

    scrollに応じてサイドメニューの色を連動

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import React from 'react'
 import Image from 'next/image'
 import { AiFillFacebook, AiFillGithub, AiFillLinkedin } from "react-icons/ai"
@@ -9,7 +9,40 @@ import { sideMenuLinks } from '@/constants/sideMenuLinks'
 
 function SideMenu() {
   const { isOpen, closeMobileMenu } = useMenuStore();
-  const { activeLink, setActiveLink } = useState(sideMenuLinks[0]);
+  const [ activeLink, setActiveLink ] = useState(sideMenuLinks[0]);
+  const handleLinkClick = (link) => {
+    closeMobileMenu();
+    setActiveLink(link);
+  };
+
+  const handleScroll = () => {
+    const sections = sideMenuLinks.map((link) =>
+      document.getElementById(link.sectionId))
+    const scrollPosition = window.scrollY
+    for (let i = sections.length - 1; i >= 0; i--) {
+      const section = sections[i]
+      if (section.offsetTop <= scrollPosition + 150) {
+        setActiveLink(sideMenuLinks[i]);
+        break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  })
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflowY = "hidden"
+    } else {
+      document.body.style.overflowY = "auto"
+    }
+  }, [isOpen])
+
   return (
     <section className={
       `fixed left-0 top-0 z-50 flex h-screen max-w-[300px] flex-col justify-between py-10 px-[20px] lg:px-16 max-lg:max-w-[146px] bg-white border border-whitesmoke
@@ -38,9 +71,8 @@ function SideMenu() {
                   className={`
               relative flex justify-center rounded-lg p-3
               ${isActive ? "bg-cadetblue text-white" : ""}
-              `}
-                >
-                  <p>{link.label}</p>
+              `} onClick={() => handleLinkClick(link)}
+                ><p>{link.label}</p>
                 </a>
               );
             })
